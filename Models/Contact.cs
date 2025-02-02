6cb2fc225df8ac84937d1cef30ff28376012753c
eden2707 <eden.lijalem-ug@aau.edu.et>
2025-02-02 09:05:08 +0300

    Update Contact.cs

--- Models/Contact.cs
+++ Models/Contact.cs
@@ -11,7 +11,7 @@ namespace ContactM.Models
         public int? Id { get; set; }     
        [Required (ErrorMessage = "Name is required.")]
         public string? Name { get; set; }  
-       [EmailAddress(ErrorMessage = "Invalid email address.")]
+       [EmailAddress(ErrorMessage = "Invalid email address.")] // Validates that the Email property contains a valid email address format and sets the error message if invalid
         public string Email { get; set; }
        [Required(ErrorMessage = "Phone number is required.")]
         [RegularExpression(@"^09\d{8}$", ErrorMessage = "Phone number must be 10 digits and start with '09'.")]
